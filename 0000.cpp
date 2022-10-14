a60366d7fdefe4a01002fc14fba0353a23329921
Riad889 <48083980+shanjidhasan@users.noreply.github.com>
2022-10-14 18:44:34 +0600

    gg

---
+++ 0000.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define ull unsigned long long
+#define pyes cout << "YES\n"
+#define pno cout << "NO\n"
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    
+    return 0;
+}
